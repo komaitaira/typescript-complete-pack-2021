32d47881e400081e1cecdee92f6b2e36247394b9
komaitaira <komaitaira@gmail.com>
2021-10-17 18:58:43 +0900

    section2 boolean, number, stringを使う方法

---
+++ section2/index.ts
@@ -0,0 +1,8 @@
+// わざわざ明示的に型注釈をつけなくても型推論をしてくれるので、基本的にはつけなくても良い。
+let hasValue = true;
+let count = 10;
+let float = 3.14;
+let negative = -0.12;
+let single = 'hello';
+let double = "hello";
+let back = `hello`;
