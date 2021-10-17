91461cadedff108d25f9ce6bd738758274bffce6
komaitaira <komaitaira@gmail.com>
2021-10-17 19:02:25 +0900

    index.tsをビルド

---
+++ section2/index.js
@@ -0,0 +1,8 @@
+// わざわざ明示的に型注釈をつけなくても型推論をしてくれるので、基本的にはつけなくても良い。
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+var double = "hello";
+var back = "hello";
