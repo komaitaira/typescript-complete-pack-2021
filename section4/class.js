1c39577849b2cddd08593e7f1c008e8c9b22edff
komaitaira <komaitaira@gmail.com>
2021-10-31 10:21:29 +0900

    finish abstractクラスを使用して継承にのみ使えるクラスを作成する

--- section4/class.js
+++ section4/class.js
@@ -1,18 +1,66 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
-    function Person(initName, initAge) {
-        this.name = initName;
-        this.age = initAge;
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
     }
+    Person.isAdult = function (age) {
+        if (age > 17)
+            return true;
+        return false;
+    };
     Person.prototype.incrementAge = function () {
         this.age += 1;
     };
     Person.prototype.greeting = function () {
         console.log("Hello My Name is " + this.name + ". I am " + this.age + " years old.");
+        this.explainJob();
     };
+    Person.species = 'Homo sapiens';
     return Person;
 }());
-var quill = new Person("Quill", 38);
-quill.greeting();
-quill.incrementAge();
-quill.greeting();
+var Teacher = /** @class */ (function (_super) {
+    __extends(Teacher, _super);
+    function Teacher(name, age, _subject) {
+        var _this = _super.call(this, name, age) || this;
+        _this._subject = _subject;
+        return _this;
+    }
+    Teacher.prototype.explainJob = function () {
+        console.log("I am a teacher and I teach " + this.subject + ".");
+    };
+    Object.defineProperty(Teacher.prototype, "subject", {
+        get: function () {
+            if (!this._subject) {
+                throw new Error("There is no subject.");
+            }
+            return this._subject;
+        },
+        set: function (value) {
+            if (!value) {
+                throw new Error("There is no subject");
+            }
+            this._subject = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Teacher;
+}(Person));
+var teacher = new Teacher("Quill", 38, "Math");
+teacher.greeting();
--- section4/class.ts
+++ section4/class.ts
@@ -1,18 +1,42 @@
-class Person {
-    name: string;
-    private age: number;
-    constructor(initName: string, initAge: number) {
-        this.name = initName;
-        this.age = initAge;
+abstract class Person {
+    static species = 'Homo sapiens';
+    static isAdult(age: number) {
+        if (age > 17) return true;
+        return false;
+    }
+    constructor(readonly name: string, protected age: number) {
     }
     incrementAge() {
         this.age += 1;
     }
     greeting(this: Person) {
-        console.log(`Hello My Name is ${this.name}. I am ${this.age} years old.`);
+        console.log(
+            `Hello My Name is ${this.name}. I am ${this.age} years old.`
+        );
+        this.explainJob()
+    }
+    abstract explainJob(): void;
+}
+
+class Teacher extends Person {
+    explainJob() {
+        console.log(`I am a teacher and I teach ${this.subject}.`);
+    }
+    get subject(): string {
+        if (!this._subject) {
+            throw new Error("There is no subject.");
+        }
+        return this._subject;
+    }
+    set subject(value) {
+        if (!value) {
+            throw new Error("There is no subject");
+        }
+        this._subject = value;
+    }
+    constructor(name: string, age: number, private _subject: string) {
+        super(name, age);
     }
 }
-const quill = new Person("Quill", 38);
-quill.greeting();
-quill.incrementAge();
-quill.greeting();
+const teacher = new Teacher("Quill", 38, "Math");
+teacher.greeting();
