c532ffa2e0096003e9bd56d2a6b7135f9a422626
komaitaira <komaitaira@gmail.com>
2021-10-31 09:29:31 +0900

    finish publickとprivateでアクセス制限

--- section4/class.js
+++ section4/class.js
@@ -1,17 +1,18 @@
 "use strict";
 var Person = /** @class */ (function () {
-    function Person(initName) {
+    function Person(initName, initAge) {
         this.name = initName;
+        this.age = initAge;
     }
+    Person.prototype.incrementAge = function () {
+        this.age += 1;
+    };
     Person.prototype.greeting = function () {
-        console.log("Hello My Name is " + this.name);
+        console.log("Hello My Name is " + this.name + ". I am " + this.age + " years old.");
     };
     return Person;
 }());
-var quill = new Person("Quill");
+var quill = new Person("Quill", 38);
 quill.greeting();
-var anotherQuill = {
-    name: 'anotherQuill',
-    greeting: quill.greeting
-};
-anotherQuill.greeting();
+quill.incrementAge();
+quill.greeting();
