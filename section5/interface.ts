a92cc296766120d823d15998b74107320ed86a43
komaitaira <komaitaira@gmail.com>
2021-11-03 17:41:32 +0900

    これがTypeScriptの構造的部分型だ

--- section5/interface.ts
+++ section5/interface.ts
@@ -6,7 +6,18 @@ interface Human {
 
 class Developer implements Human {
     constructor(public name: string, public age: number, public experience: number) { }
+    greeting(message: string): void {
+        console.log(message);
+    }
+}
+
+const tmpDeveloper = {
+    name: 'Quill',
+    age: 38,
+    experience: 3,
     greeting(message: string) {
         console.log(message);
     }
 }
+// const user = new Developer('Quill', 38, 3);
+const user: Human = tmpDeveloper;
