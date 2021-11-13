380e4a639580ad02cd90cabd9a4616e61b29c339
komaitaira <komaitaira@gmail.com>
2021-11-13 23:35:18 +0900

    戻り値にクラスを指定して、新しいクラスを作り出す方法

--- section8/decorator.js
+++ section8/decorator.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -15,17 +30,30 @@ function Logging(message) {
 function Component(template, selector) {
     console.log("Component Factory");
     return function (constructor) {
-        var mountedElement = document.querySelector(selector);
-        console.log("Component");
-        var instance = new constructor();
-        if (mountedElement) {
-            mountedElement.innerHTML = template;
-            mountedElement.querySelector("h1").textContent = instance.name;
-        }
+        return /** @class */ (function (_super) {
+            __extends(class_1, _super);
+            function class_1() {
+                var args = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    args[_i] = arguments[_i];
+                }
+                var _this = _super.apply(this, args) || this;
+                console.log("Component");
+                var mountedElement = document.querySelector(selector);
+                var instance = new constructor();
+                if (mountedElement) {
+                    mountedElement.innerHTML = template;
+                    mountedElement.querySelector("h1").textContent = instance.name;
+                }
+                return _this;
+            }
+            return class_1;
+        }(constructor));
     };
 }
 var User = /** @class */ (function () {
-    function User() {
+    function User(age) {
+        this.age = age;
         this.name = "Quill";
         console.log("User was created!");
     }
@@ -35,6 +63,6 @@ var User = /** @class */ (function () {
     ], User);
     return User;
 }());
-var user1 = new User();
-var user2 = new User();
-var user3 = new User();
+var user1 = new User(32);
+var user2 = new User(32);
+var user3 = new User(32);
