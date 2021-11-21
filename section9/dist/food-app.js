e0c900633214ba10afec1f253f00a45f61752465
komaitaira <komaitaira@gmail.com>
2021-11-21 21:39:30 +0900

    activeな食べ物のスコアの配列を取得できるようにする方法

--- section9/dist/food-app.js
+++ section9/dist/food-app.js
@@ -7,25 +7,36 @@ class Food {
         element.addEventListener("click", this.clickEventHandler.bind(this));
     }
     clickEventHandler() {
-        this.element.classList.toggle('food--active');
+        this.element.classList.toggle("food--active");
     }
 }
 class Foods {
     constructor() {
         this.elements = document.querySelectorAll(".food");
         this._activeElements = [];
-        this.elements.forEach(element => {
+        this._activeElementsScore = [];
+        this.elements.forEach((element) => {
             new Food(element);
         });
     }
     get activeElements() {
         this._activeElements = [];
-        this.elements.forEach(element => {
-            if (element.classList.contains('food--active')) {
+        this.elements.forEach((element) => {
+            if (element.classList.contains("food--active")) {
                 this._activeElements.push(element);
             }
         });
         return this._activeElements;
+    }
+    get activeElementsScore() {
+        this._activeElementsScore = [];
+        this.activeElements.forEach((element) => {
+            const foodScore = element.querySelector(".food__score");
+            if (foodScore) {
+                this._activeElementsScore.push(Number(foodScore.textContent));
+            }
+        });
+        return this._activeElementsScore;
     }
 }
 const foods = new Foods();
