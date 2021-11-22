7ef9c45e11d58b8c1d64953190563813a2a0176e
komaitaira <komaitaira@gmail.com>
2021-11-23 00:00:18 +0900

    namespaceで型を定義する方法

--- section10/src/library.ts
+++ section10/src/library.ts
@@ -1,4 +1,12 @@
 // import axios from "axios";
 // axios.get('https://fooapi.com')
 // import _ from 'lodash';
-console.log(_.shuffle([1, 2, 3, 4]))
+// console.log(_.shuffle([1, 2, 3, 4]));
+namespace myApp {
+    const hello = "hello in namespace";
+    export const name = "Quill";
+    export interface Nameable {
+        name: string;
+    }
+}
+let nameable: myApp.Nameable;
--- section10/src/lodash.d.ts
+++ section10/src/lodash.d.ts
@@ -1,6 +1,10 @@
 declare module "lodash" {
     export function shuffle<T>(arr: T[]): T[]
 }
-declare const _: {
-    shuffle<T>(arr: T[]): T[]
+// interface Lodash {
+//     shuffle<T>(arr: T[]): T[]
+// }
+
+declare namespace _ {
+    function shuffle<T>(arr: T[]): T[]
 }
